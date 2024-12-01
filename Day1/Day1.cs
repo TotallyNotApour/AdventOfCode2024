a0735e19f4e536d3fd6c6bc2704400f333eb95ae
TotallyNotApour <leonard.lesage1@gmail.com>
2024-12-01 15:54:57 -0500

    Day1 Finished

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -1,9 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
 
-class Program
+class Day1
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello, tiqqwe!");
+        Puzzle2();
+
+    }
+
+    static void Puzzle1(){
+        string path = @"C:\MyProjects\AdventOfCode2024\Day1\PuzzleInput.txt";
+
+        List<int> list1 = new();
+        List<int> list2 = new();
+
+        foreach (var line in File.ReadLines(path))
+        {
+            var split = line.Split("   ");
+
+            list1.Add(Int32.Parse(split[0]));
+            list2.Add(Int32.Parse(split[1]));
+        }
+
+        list1.Sort();
+        list2.Sort();
+
+        int sizeList1 = list1.Count();
+        int sizeList2 = list1.Count();
+
+        int TotalDistance = 0;
+
+        if (sizeList1 == sizeList2){
+            for (int i = 0; i < sizeList1; i++){
+                int num1 = list1[i];
+                int num2= list2[i];
+
+                TotalDistance += Math.Abs(num1 - num2);
+            }
+            Console.WriteLine("The Total Distance between the two list is : " + TotalDistance);
+        }
+        else {
+            Console.WriteLine("ERROR");
+        }
+    }
+
+
+    static void Puzzle2(){
+        string path = @"C:\MyProjects\AdventOfCode2024\Day1\PuzzleInput.txt";
+
+        List<int> list1 = new();
+        List<int> list2 = new();
+
+        foreach (var line in File.ReadLines(path))
+        {
+            var split = line.Split("   ");
+
+            list1.Add(Int32.Parse(split[0]));
+            list2.Add(Int32.Parse(split[1]));
+        }
+
+        list1.Sort();
+        list2.Sort();
+
+        int sizeList1 = list1.Count();
+        int sizeList2 = list1.Count();
+
+        int totalSimilarityScore = 0;
+
+        if (sizeList1 == sizeList2){
+
+            for (int i = 0; i < sizeList1; i++){
+                int num1 = list1[i];
+
+                int similarityScore = num1 * list2.Count(n => n == num1);
+
+                totalSimilarityScore += similarityScore;
+            }
+            Console.WriteLine("The Total Similarity between the two list is : " + totalSimilarityScore);
+        }
+        else {
+            Console.WriteLine("ERROR");
+        }
     }
 }
